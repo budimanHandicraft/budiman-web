68961f5ac4cf463dd1587a7fe4fdf2e4c6f36345
ibnu-zaidan-akbar <akbaribnu031@gmail.com>
2026-06-16 08:10:57 +0700

    fix: bug middleware

--- budiman-handicraft/middleware.ts
+++ budiman-handicraft/middleware.ts
@@ -1,13 +1,12 @@
 import { NextResponse } from 'next/server';
-import { NextRequest } from 'next/server';
+import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-    const url = request.nextUrl;
-    const adminRoute = url.pathname.startsWith('/admin')
+    const { pathname, searchParams } = request.nextUrl;
 
-    if(adminRoute){
+    if(pathname.startsWith('/admin')){
         // jika terbobol akses nya, ganti password (secret) dan nama cookie nya (admin_granted) yaaa
-        const secretKey = url.searchParams.get('secret')
+        const secretKey = searchParams.get('secret')
         const hasAdminCookie = request.cookies.get('admin_granted')
 
         if(secretKey == 'token_admin'){
@@ -16,6 +15,7 @@ export function middleware(request: NextRequest) {
             response.cookies.set('admin_granted', 'true', {
                 httpOnly: true,
                 secure: process.env.NODE_ENV === 'production',
+                path:'/',
                 maxAge: 60 * 60 * 24 * 7,
             })
             return response
@@ -24,11 +24,15 @@ export function middleware(request: NextRequest) {
         if(hasAdminCookie?.value === 'true'){
             return NextResponse.next()
         }
-        return NextResponse.redirect(new URL('/', request.url))
+        const homeUrl = request.nextUrl.clone();
+        homeUrl.pathname = '/';
+        homeUrl.search = '';
+
+        return NextResponse.redirect(homeUrl)
     }
     return NextResponse.next()
 }
 
 export const config = {
-    matcher: '/admin/:path*',
+    matcher: ['/admin', '/admin/:path*'],
 }
