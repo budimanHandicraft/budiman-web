62a70f899f24fa2a8f7b796daa6104cb44bdf8d5
budimanHandicraft <budimanhandicraft7@gmail.com>
2026-06-16 08:42:45 +0700

    fix: middleware 1.2

--- budiman-handicraft/middleware.ts
+++ budiman-handicraft/middleware.ts
@@ -2,37 +2,38 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-    const { pathname, searchParams } = request.nextUrl;
+    const pathname = request.nextUrl.pathname;
+    const isAdminRoute = pathname.startsWith('/admin');
 
-    if(pathname.startsWith('/admin')){
-        // jika terbobol akses nya, ganti password (secret) dan nama cookie nya (admin_granted) yaaa
-        const secretKey = searchParams.get('secret')
-        const hasAdminCookie = request.cookies.get('admin_granted')
-
-        if(secretKey == 'token_admin'){
-            const response = NextResponse.next()
-
+    if (isAdminRoute) {
+        const secretKey = request.nextUrl.searchParams.get('secret');
+        const hasAdminCookie = request.cookies.get('admin_granted');
+        if (secretKey === 'token_admin') {
+            const response = NextResponse.next();
+            
             response.cookies.set('admin_granted', 'true', {
                 httpOnly: true,
                 secure: process.env.NODE_ENV === 'production',
-                path:'/',
                 maxAge: 60 * 60 * 24 * 7,
-            })
-            return response
+                path: '/',
+            });
+            return response;
         }
 
-        if(hasAdminCookie?.value === 'true'){
-            return NextResponse.next()
+        if (hasAdminCookie?.value === 'true') {
+            return NextResponse.next();
         }
-        const homeUrl = request.nextUrl.clone();
-        homeUrl.pathname = '/';
-        homeUrl.search = '';
 
-        return NextResponse.redirect(homeUrl)
+        const loginUrl = request.nextUrl.clone();
+        loginUrl.pathname = '/';
+        loginUrl.search = '';
+        
+        return NextResponse.redirect(loginUrl);
     }
-    return NextResponse.next()
+
+    return NextResponse.next();
 }
 
 export const config = {
-    matcher: ['/admin', '/admin/:path*'],
-}
+    matcher: '/admin/:path*',
+};
